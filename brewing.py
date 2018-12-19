812268b1d7733e3ce75e8702a24f99140d7b7f4b
Adrian D'Alessandro <adr.dalessandro@gmail.com>
2018-12-19 17:35:16 +1100

    slowly making OO-like changes

--- brewing.py
+++ brewing.py
@@ -2,51 +2,72 @@ import json
 import math
 
 class BeerRecipe(object):
-    def __init__(self, batch_size, grain_mass, trub_loss, kettle_loss, boil_time, bottle_size):
+    def __init__(self, batch_size=20, grain_mass=5, trub_loss=0,
+                 kettle_loss=0, boil_time=60, bottle_size=450):
         
         # Define some constants
-        constants = {
-            "Evaporation Rate" : 3, # Approximate rate of evaporation at boil (L/hr)
-            "Grain Absorption Rate" : 1, # Approximate loss of volume due to grain absorption (L/kg)
-            "Efficiency" : 0.5 # Efficiency of getting sugars from malt
+        self.constants = {
+            "Evaporation Rate" : 3, # During boil (L/hr)
+            "Grain Absorption Rate" : 1, # Volume loss from grain (L/kg)
+            "Efficiency" : 0.6, # Efficiency of getting sugars from malt (%)
+            "Fermentability" : 0.75
         }
         
-        # ### Calculate Water amounts
-        # Need functions to take in batch size and work out the amounts all the way to strike water, inculding assumptions for losses etc.
-        # Calculations and definitions from [Mash Hacks](https://mashhacks.com/how-to-calculate-water-volumes-for-brewing/):
-        # 
-        # - **Strike Water** - The starting amount of hot water (usually between 60C and 80C) that is used for the mash (adding grain to water).
-        # - **Grain Absorption** - The amount of water absorbed by the grain.
-        # - **First Runnings** - The amount of wort (sugary water) that is collected from the mash tun after your mash has finished.
-        # - **Sparge Water** - Water that is added to the mash tun after mashing to rinse the grain of any left over sugars.
-        # - **Pre-Boil Volume** - The amount of wort (sugary water) that is in your kettle before you start your boil.
-        # - **Evaporation Rate** - The amount of water that is boiled off. Usually in Litres per Hour (or Gal/hr).
-        # - **Post-Boil Volume** - The amount of wort left after you finish the boil.
-        # - **Kettle Loss** - The amount of wort (if any) left at the bottom of the kettle that was not transferred to the fermenter.
-        # - **Fermenter Volume** - The amount of wort you were able to get out of the kettle and put into the fermenter.
-        # - **Trub Loss** aka *Fermenter loss* - The amount of beer (if any) at the bottom of the fermenter that was not transferred into bottles or kegs.
-        # - **Batch Size** - The amount of beer that you were able to get out of the fermenter and put into bottles or kegs.
+        self.batch_size = batch_size
+        self.grain_mass = grain_mass
+        self.trub_loss = trub_loss
+        self.kettle_loss = kettle_loss
+        self.boil_time = boil_time
+        self.bottle_size = bottle_size
+        
+        # Calculate Water amounts:
+        # Need functions to take in batch size and work out the amounts all the
+        # way to strike water, inculding assumptions for losses etc.
+        # Calculations and definitions from Mash Hacks:
+        # https://mashhacks.com/how-to-calculate-water-volumes-for-brewing/):
 
-        # Calculte Water Volumes
-        fermenter_vol = batch_size + trub_loss
-        post_boil_vol = fermenter_vol + kettle_loss
-        pre_boil_vol = post_boil_vol + constants["Evaporation Rate"] * boil_time
+        # Different Values:
+        # - **Strike Water** (calculated)
+        # - **Grain Absorption** (calculated)
+        # - **First Runnings** (calculated)
+        # - **Sparge Water** (calculated)
+        # - **Pre-Boil Volume** (calculated)
+        # - **Evaporation Rate** (constant)
+        # - **Post-Boil Volume** (calculated)
+        # - **Kettle Loss** (provided or estimated by default %val)
+        # - **Fermenter Volume**  (calculated)
+        # - **Trub Loss** aka *Fermenter loss* (provided or estimated)
+        # - **Batch Size** (provided)
 
-        strike_water = 3 * grain_mass
-        grain_absorption = constants["Grain Absorption Rate"] * grain_mass
-        first_runnings = strike_water - grain_absorption
+        # Calculate Water Volumes
+        self.fermenter_vol = batch_size + trub_loss
+        self.post_boil_vol = self.fermenter_vol + kettle_loss
+        self.pre_boil_vol = (self.post_boil_vol
+                             + self.constants["Evaporation Rate"]
+                             * boil_time)
 
-        sparge_water = pre_boil_vol - first_runnings
+        self.strike_water = 3 * grain_mass
+        self.grain_absorption = (self.constants["Grain Absorption Rate"]
+                                 * grain_mass)
+        self.first_runnings = self.strike_water - self.grain_absorption
+
+        self.sparge_water = self.pre_boil_vol - self.first_runnings
 
         
-        self.original_gravity = 1.044
-        self.final_gravity = 1.01
+        self.original_gravity = self.hwe2gravity(
+            self.expected_original_hwe(self.grain_mass, 0.8)
+        )
+        self.final_gravity = (self.original_gravity
+                              - (self.original_gravity - 1)
+                              * self.constants["Fermentability"])
 
-        prc_vol = self.alcohol_percentage(self.original_gravity, self.final_gravity)
-        std_drinks = self.standard_drinks(bottle_size, prc_vol)
+        self.prc_vol = self.alcohol_percentage(self.original_gravity,
+                                               self.final_gravity)
+        self.std_drinks = self.standard_drinks(bottle_size, self.prc_vol)
 
-        print(f"Final alcohol percentage = {prc_vol:.2}% ABV")
-        print(f"{std_drinks:.2} Standard Drinks per {bottle_size}mL bottle")
+        print(f"Final alcohol percentage = {self.prc_vol:.2}% ABV")
+        print(f"{self.std_drinks:.2} Standard Drinks per {bottle_size}"
+              + "mL bottle")
         
     def alcohol_percentage(self, original_gravity, final_gravity):
         return (original_gravity - final_gravity) * 131.25
@@ -56,15 +77,18 @@ class BeerRecipe(object):
     
     def expected_original_hwe(self, grain_mass, malt_hwe):
         """
-        # ### Calculate Expected Original Gravity
+        Calculate Expected Original Gravity
 
-        # Mash Efficiency - from https://aussiehomebrewer.com/threads/working-out-mash-efficiency-in-metric.35490/
-        # The H.W.E (hot water extract) value for the malt is the amount of sugar in the wort
-        # 386 is the maximum (pure sugar) so every malt is given as a % of that
+        Mash Efficiency - Calculations from:
+        https://aussiehomebrewer.com/threads/working-out-mash-efficiency-in-metric.35490/
+        
+        The H.W.E (hot water extract) value for the malt is the amount of
+        sugar in the wort 386 is the maximum (pure sugar) so every malt is
+        given as a % of that
         """
-        potential_hwe = grain_mass * 386 * malt_hwe / batch_size
+        potential_hwe = grain_mass * 386 * malt_hwe / self.post_boil_vol
 
-        original_hwe = 0.5 * potential_hwe
+        original_hwe = self.constants["Efficiency"] * potential_hwe
 
         return original_hwe
 
@@ -80,18 +104,20 @@ class BeerRecipe(object):
 
         return gravity
 
-    def ibu(self, mass_hops, batch_size, aa_rating, boil_gravity=None, boil_time=60):
+    def calculate_ibu(self, mass_hops, batch_size, aa_rating,
+            boil_gravity=None, boil_time=60):
         """
-        # ### Calculate IBU
-        # http://www.backtoschoolbrewing.com/blog/2016/9/5/how-to-calculate-ibus
-        # http://www.realbeer.com/hops/research.html
-        # 
-        # To calculate IBUs, the formula is:
-        # `IBUs = decimal alpha acid utilization * mg/l of added alpha acids`
-        # `mg/l of added alpha acids = decimal AA rating * grams hops * 1000 / volume of finished beer in liters`
-        # `decimal alpha acid utilization = Bigness factor * Boil Time factor`
-        # `Bigness factor = 1.65 * 0.000125^(wort gravity - 1)`
-        # `Boil Time factor = (1 - e^(-0.04 * time in mins) ) / 4.15`
+        Calculate IBU
+        http://www.backtoschoolbrewing.com/blog/2016/9/5/how-to-calculate-ibus
+        http://www.realbeer.com/hops/research.html
+        
+        To calculate IBUs, the formula is:
+        `IBUs = decimal alpha acid utilization * mg/l of added alpha acids`
+        `mg/l of added alpha acids = decimal AA rating * grams hops * 1000
+                                     / volume of finished beer in liters`
+        `decimal alpha acid utilization = Bigness factor * Boil Time factor`
+        `Bigness factor = 1.65 * 0.000125^(wort gravity - 1)`
+        `Boil Time factor = (1 - e^(-0.04 * time in mins) ) / 4.15`
         """
         if boil_gravity is None:
             boil_gravity = self.original_gravity
@@ -103,31 +129,39 @@ class BeerRecipe(object):
         aa_utilization = bigness_factor * boil_time_factor
 
         aa_concentration = aa_rating / 100 * mass_hops * 1000 / batch_size
-
+        
         return aa_utilization * aa_concentration
 
     def ebc(self, grain_mass, malt_colour, batch_size):
         """
-        # ### Calculate EBC
-        # http://beersmith.com/blog/2008/04/29/beer-color-understanding-srm-lovibond-and-ebc/
-        # http://homebrewtechniques.com/mashing/how-do-you-calculate-beer-colour/
-        # 
-        # The calculation we use to work out the colour value for our beer is:
-        # MCU = weight of malt (in lbs) x malt colour (in lovibond) / volume (in gallons)
-        # (lovibond = SRM)
-        # To convert our recipe from kg and litres into US gallons and lbs we use the following conversion factors:
-        # 1 litre = 0.264172 US gallons
-        # 1 kg = 2.20462 lbs.
-        # SRM = 1.4922 * MCU^0.6859
-        # EBC = 1.97 * SRM 
+        Calculate EBC
+        http://beersmith.com/blog/2008/04/29/beer-color-understanding-srm-lovibond-and-ebc/
+        http://homebrewtechniques.com/mashing/how-do-you-calculate-beer-colour/
+        
+        The calculation we use to work out the colour value for our beer is:
+        MCU = weight of malt (in lbs) * malt colour (lovibond)
+              / volume (in gallons)
+        
+        (lovibond = SRM)
+        To convert our recipe from kg and litres into US gallons and lbs we
+        use the following conversion factors:
+        
+        1 litre = 0.264172 US gallons
+        1 kg = 2.20462 lbs.
+        SRM = 1.4922 * MCU^0.6859
+        EBC = 1.97 * SRM 
         """
-        return 1.97 * (1.4922 * ((grain_mass*2.2*malt_colour/1.97)/(batch_size*0.264172))**0.6859)
+        mcu = (grain_mass * 2.2 * malt_colour / 1.97) / (batch_size * 0.264172)
+        
+        srm = 1.4922 * mcu**0.6859
+        
+        return 1.97 * srm
     
     def read_recipe(self, recipe_file):
         with open(recipe_file) as f:
             self.json = json.load(f)
         return self.json
     
-    def write_recipe(self, recipe_file):
+    def save_recipe(self, recipe_file):
         with open(recipe_file, 'w') as f:
             json.dump(self.json, f, indent=4)
